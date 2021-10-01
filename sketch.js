e443a01e5531f6a36e617fe4c7055b86a75209d0
swapnilsakhare123 <83084295+swapnilsakhare123@users.noreply.github.com>
2021-10-01 17:53:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,12 +52,7 @@ function draw() {
   }
 }
 
-   if(spacecraft.y <= (iss.y+180) && spacecraft.x <= (iss.x-10)){
-     hasDocked = true;
-     textSize(25);
-     fill("white")
-     text("Docking Successful!", 200, 300);
-   }
+   
 
   // if(spacecraft.y <= (iss.y-70) && spacecraft.x <= (iss.x-10)){4
   //   hasDocked = true;
@@ -73,12 +68,12 @@ function draw() {
   //   text("Docking UnSuccessful!", 200, 300);
   // }
 
-  // if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
-  //   hasDocked = true;
-  //   textSize(25);
-  //   fill("white")
-  //   text("Docking Successful!", 200, 300);
-  // }
+   if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
+     hasDocked = true;
+     textSize(25);
+     fill("white")
+     text("Docking Successful!", 200, 300);
+   }
 
   drawSprites();
 }
